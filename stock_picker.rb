73307a04bfda65e27d31930bc319bf42afb229b2
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-02-23 21:17:43 +0000

    Completed the stock picker again to make sure I understand it.

--- stock_picker.rb
+++ stock_picker.rb
@@ -10,13 +10,17 @@ def stock_picker(stock_prices)
   stock_prices.each_with_index do |buy_price, buy_day|
     # We need a nested loop inside of here to compare each possible
     # pair of buy and sell days.
+
+    # Start nested loop at the buy_day + 1 so we can only sell after
+    # we buy. A range between that and the total length of the array
+    # using an inclusive range.
     ((buy_day + 1)...stock_prices.length).each do |sell_day|
       sell_price = stock_prices[sell_day]
       profit = sell_price - buy_price
       if profit > max_profit
-        max_profit = profit
-        best_buy = buy_day
-        best_sell = sell_day
+        max_profit = profit # Update the max profit to the new highest
+        best_buy = buy_day # this will repeat until the loop ends
+        best_sell = sell_day # by then it will have found the best days with highest profit.
       end
     end
   end
